425a6ff91a32e3e9cdd4a57473ed71983bd84433
sb-haz <sb_haz@icloud.com>
2021-12-01 03:46:31 +0000

    Generate png output

---
+++ word_cloud.py
@@ -0,0 +1,24 @@
+
+# Imports 
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import sys, os
+os.chdir(sys.path[0])
+
+# Read text
+text = open('text.txt', mode='r', encoding='utf-8').read()
+stopwords = STOPWORDS
+
+# WordCloud
+wc = WordCloud(
+    background_color = 'white',
+    stopwords = stopwords,
+    height = 1000,
+    width = 1000
+)
+
+# Generate
+wc.generate(text)
+ 
+# Store to file
+wc.to_file('output.png')

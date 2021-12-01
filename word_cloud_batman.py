cea933f275547b6a5ca6f2bf1292eed0f347a718
sb-haz <sb_haz@icloud.com>
2021-12-01 21:44:26 +0000

    Batman image output

--- word_cloud_batman.py
+++ word_cloud_batman.py
@@ -5,14 +5,17 @@ import numpy as np # get colour of image
 
 # Content-related
 text = open('batman.txt', 'r', encoding='utf-8').read()
+stopwords = STOPWORDS
 
 # Appearance-related
 wc = WordCloud(
     background_color = 'white',
-    
+    stopwords = stopwords,
+    height = 1000,
+    width = 1000    
 )
 # Generate
 wc.generate(text)
 
 # Store to file
-wc.to_file('output.png')
+wc.to_file('batman_output.png')

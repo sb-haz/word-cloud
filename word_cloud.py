f034b73fef959230af44b1ba4175c88ba54639f2
sb-haz <sb_haz@icloud.com>
2021-12-03 17:21:42 +0000

    refactor

--- word_cloud.py
+++ word_cloud.py
@@ -1,22 +1,35 @@
 # Imports 
-from wordcloud import WordCloud, STOPWORDS
-import sys, os
-os.chdir(sys.path[0])
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image # load image
+import numpy as np # get colour of image
 
-# Read text
-text = open('text.txt', mode='r', encoding='utf-8').read()
+# Content-related
+text = open('batman.txt', 'r', encoding='utf-8').read()
 stopwords = STOPWORDS
 
-# WordCloud
+# Mask
+custom_mask = np.array(Image.open('batman.png'))
+
+# WordCloud attributes
 wc = WordCloud(
-    background_color = 'white',
+    font_path = 'SFProDisplay-Light.ttf',
+    #margin = 10,
+    mask = custom_mask,
+    background_color = 'black',
+    #background_color = None,
+    #mode = 'RGBA',
     stopwords = stopwords,
     height = 1000,
-    width = 1000
+    width = 1000,
+    include_numbers = False, # include numbers
+    #color_func=lambda *args, **kwargs: (255,255,255) # text colour
 )
-
 # Generate
 wc.generate(text)
- 
+
+# Use colour of mask image
+## image_colours = ImageColorGenerator(custom_mask)
+## wc.recolor(color_func = image_colours)
+
 # Store to file
-wc.to_file('output.png')
+wc.to_file('batman_output.png')
